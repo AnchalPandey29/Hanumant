48ee32b5802de6f6a97d807f686bd063151a0ad3
Anchal Pandey <anchal29pandey@gmail.com>
2025-09-27 12:44:20 +0000

    Add any icon or image as of now for favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><path d=%22M42 20 L58 52 L72 32 L90 80 H10 L42 20 Z%22 fill=%22%23226688%22 /></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;900&display=swap" rel="stylesheet" />
