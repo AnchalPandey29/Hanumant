4a7558ec20a76b2b0d69c2478639b8bee0fe1096
Anchal Pandey <anchal29pandey@gmail.com>
2025-09-27 12:43:39 +0000

    Change the favicon and title to hanumant technology It should not feel f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'Hanumant Technology | Redesign',
+  title: 'Hanumant Technology',
   description: 'Empowering the next generation of tech talent.',
 };
 
