cb8976a469cb37ad19877e2de0ce794059fc7a70
Anchal Pandey <anchal29pandey@gmail.com>
2025-09-27 10:32:01 +0000

    still the line is visible

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -91,7 +91,7 @@ export default function HeroSection() {
         </div>
       </div>
 
-      <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-[0]">
+      <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-[0] -mt-px">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1440 320"
